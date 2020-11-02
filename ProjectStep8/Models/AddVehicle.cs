6083f49eeb5e0a63d72fd7c2f52e6a03bd7f1467
mattfoster2 <matt.foster2@outlook.com>
2020-11-02 09:26:57 -0600

    Initial Repository

---
+++ ProjectStep8/Models/AddVehicle.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.VisualBasic;
+using ProjectStep8.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectStep8.Models
+{
+   public class AddVehicle
+   {
+      // F i e l d s   &   P r o p e r t i e s 
+      public int Year { get; set; }
+      public string Make { get; set; }
+      public string Model { get; set; }
+      public string Trim { get; set; }
+      public int Mileage { get; set; }
+      public DateTime PurchaseDate { get; set; }
+      public decimal PurchasePrice { get; set; }
+      public string EngineType { get; set; }
+      public string EngineDescription { get; set; }
+      public string Horsepower { get; set; }
+      public string Torque { get; set; }
+      public string TransmissionType { get; set; }
+      public string DrivetrainType { get; set; }
+      public string SteeringType { get; set; }
+      public string FrontSuspensionType { get; set; }
+      public string RearSuspensionType { get; set; }
+      public string BrakeType { get; set; }
+      public string AboutVehicle { get; set; }
+
+      //  C o n s t r u c t o r s 
+
+      //  M e t h o d s 
+   }
+}
